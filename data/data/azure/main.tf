1d87c93e8f6f6daebf5f293061ffdc713e995957
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2021-08-23 08:41:43 +0300

    refactor!: remove service principal from provider

--- data/data/azure/main.tf
+++ data/data/azure/main.tf
@@ -14,8 +14,8 @@ locals {
 provider "azurerm" {
   features {}
   subscription_id = var.azure_subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
+#  client_id       = var.azure_client_id
+#  client_secret   = var.azure_client_secret
   tenant_id       = var.azure_tenant_id
   environment     = var.azure_environment
 }
